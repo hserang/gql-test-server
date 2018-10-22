90645354ae20295bea7a1321da652a23caebaa51
Haroun Serang <haroun@zenput.com>
2018-10-21 20:10:22 -0700

    Change port to 4000

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Start the server
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('Go to http://localhost:3000/graphiql to run queries!');
 });
